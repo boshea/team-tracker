f089a14c77ee888d82930aad13b1410c86db8986
Brian <boshea@salesforce.com>
2019-02-17 14:54:56 -0800

    Initial checkin of Team Tracker web UI code.

---
+++ web-ui/src/components/about/AboutPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>
+                    This application helps Alzheimer&quot;s Association volunteers
+                    stay in touch with and reach out to the captains of teams that
+                    they support during their fundraising campaign for the Walk to
+                    End Alzheimer&quot;s.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
---
+++ web-ui/src/components/emptyTest.spec.js
@@ -0,0 +1,10 @@
+// Must have at least one test file in this directory or Mocha will throw an error.
+import expect from 'expect';
+
+describe('Sample Tests', () => {
+    describe('Sample', () => {
+        it('should return true', () => {
+            expect(true).toEqual(true);
+        });
+    });
+});
